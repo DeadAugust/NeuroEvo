350f26fd05c82f00128c5f1c268ac469b7f85e33
DeadAugust <augustluhrs@gmail.com>
2019-03-31 20:24:37 -0400

    first commit

---
+++ FlappyBrain/bird.js
@@ -0,0 +1,143 @@
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
+
+// This flappy bird implementation is adapted from:
+// https://youtu.be/cXgA1d_E-jY&
+
+// Mutation function to be passed into bird.brain
+//effects the two weight matrices, and the two bias matrices
+/*
+function mutate(x) {
+  if (random(1) < 0.1) {
+    let offset = randomGaussian() * 0.5;
+    let newx = x + offset;
+    return newx;
+  } else {
+    return x;
+  }
+}
+*/
+
+//now just passes the scalar value to add
+function mutation() { //changed to mutation for clarity
+  let mutationScalar = 0;
+  if (random(1) < 0.5) {
+    let offset = randomGaussian() * 0.5;
+    mutationScalar += offset;
+    console.log(mutationScalar + 'offset');
+  }
+  return mutationScalar;
+}
+
+class Bird {
+  constructor(brain) {
+    // position and size of bird
+    this.x = 64;
+    this.y = height / 2;
+    this.r = 12;
+
+    // Gravity, lift and velocity
+    this.gravity = 0.8;
+    this.lift = -12;
+    this.velocity = 0;
+
+    // Is this a copy of another Bird or a new one?
+    // The Neural Network is the bird's "brain"
+    if (brain instanceof NeuralNetwork) {
+      this.brain = brain.copy();
+      this.brain.mutate(mutation());
+    } else {
+      this.brain = new NeuralNetwork(5, 8, 2, 'sigmoid', 'meanSquaredError', 'sgd'); //update later with other act_func options
+    }
+
+    // Score is how many frames it's been alive
+    this.score = 0;
+    // Fitness is normalized version of score
+    this.fitness = 0;
+  }
+
+  // Create a copy of this bird
+  copy() {
+    return new Bird(this.brain);
+  }
+
+  // Display the bird
+  show() {
+    fill(255, 100);
+    stroke(255);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  // This is the key function now that decides
+  // if it should jump or not jump!
+  async think(pipes) {
+    // First find the closest pipe
+    let closest = null;
+    let record = Infinity;
+    for (let i = 0; i < pipes.length; i++) {
+      let diff = pipes[i].x - this.x;
+      if (diff > 0 && diff < record) {
+        record = diff;
+        closest = pipes[i];
+      }
+    }
+
+    if (closest != null) {
+      // Now create the inputs to the neural network
+      let inputs = [];
+      // x position of closest pipe
+      inputs[0] = map(closest.x, this.x, width, 0, 1);
+      // top of closest pipe opening
+      inputs[1] = map(closest.top, 0, height, 0, 1);
+      // bottom of closest pipe opening
+      inputs[2] = map(closest.bottom, 0, height, 0, 1);
+      // bird's y position
+      inputs[3] = map(this.y, 0, height, 0, 1);
+      // bird's y velocity
+      inputs[4] = map(this.velocity, -5, 5, 0, 1);
+
+      // Get the outputs from the network (an array of 2)
+      // let action = this.brain.predict(inputs);
+      // let action;
+      let test = 0;
+      // async function decide(){
+      let action = await this.brain.predict(inputs);
+      //   test++;
+      // }
+      // let action = async () => {
+      //   test++;
+      //   console.log('this');
+      //   return this.brain.predict(inputs);
+      // }
+      // action();
+      // Decide to jump or not!
+      // console.log(tf.print(action));
+      // console.log(action[0]);
+      if (action[1] > action[0]) { //WHAT, that's crazy
+        this.up();
+      }
+    }
+  }
+
+  // Jump up
+  up() {
+    // console.log('jump!');
+    this.velocity += this.lift;
+  }
+
+  bottomTop() {
+    // Bird dies when hits bottom?
+    return (this.y > height || this.y < 0);
+  }
+
+  // Update bird's position based on velocity, gravity, etc.
+  update() {
+    this.velocity += this.gravity;
+    // this.velocity *= 0.9;
+    this.y += this.velocity;
+
+    // Every frame it is alive increases the score
+    this.score++;
+  }
+}
---
+++ FlappyBrain/scratch.js
@@ -0,0 +1,178 @@
+//updated to use tf.js
+class NeuralNetwork {
+  constructor(in_nodes, hid_nodes, out_nodes, act_func, loss_func, optimizer_) {
+    if (in_nodes instanceof NeuralNetwork) {
+      let oldBrain = in_nodes;
+      //need all this repetition?
+      this.input_nodes = oldBrain.input_nodes;
+      this.hidden_nodes = oldBrain.hidden_nodes;
+      this.output_nodes = oldBrain.output_nodes;
+      this.activation_function = oldBrain.activation_function;
+      this.loss_function = oldBrain.activation_function;
+      this.optimizer = oldBrain.optimizer;
+      //really just this, right?
+      this.model = oldBrain.model;
+
+    } else { //new NN
+      this.input_nodes = in_nodes; //why the repetition? to copy
+      this.hidden_nodes = hid_nodes;
+      this.output_nodes = out_nodes;
+      this.activation_function = act_func;
+      this.loss_function = loss_func;
+      this.optimizer = optimizer_;
+      tf.tidy(() => {
+        const model = tf.sequential();
+        //adding hidden layer fed from input layer
+        model.add(tf.layers.dense({
+          units: this.hidden_nodes,
+          inputShape: [this.input_nodes],
+          activation: this.activation_function
+          //kernel - weights?
+          //bias? useBias = true;
+        }));
+        //adding output layer fed from hidden layer
+        model.add(tf.layers.dense({
+          units: this.output_nodes,
+          activation: this.activation_function
+        }));
+        //already randomized, so no need to call .randomize();
+        //prepare the model
+        model.compile({
+          loss: this.loss_function,
+          optimizer: this.optimizer
+        });
+        //no need to fit, because that's what the GA is for
+
+        this.model = model;
+      });
+    }
+  }
+
+  async predict(input_array) {
+    const actionOutputs = tf.tidy(() => {
+      const inputs = tf.tensor2d([input_array]);
+      return this.model.predict(inputs); //await?
+    });
+    let actionArray = await actionOutputs.array();
+    tf.dispose(actionOutputs); //fixed memory leak
+    // console.log(actionArray[0]);
+    return actionArray[0];
+  }
+
+  // still need this? yes, for bird.js line 35
+  copy() {
+    return new NeuralNetwork(this);
+  }
+
+  // Accept an arbitrary function for mutation (bird.js line 10)
+  //map is
+  mutate(m) {
+    // this.weights_ih.map(func);
+    // // value = value + offset
+    // this.weights_ho.map(func);
+    // this.bias_h.map(func);
+    // this.bias_o.map(func);
+    if (m != 0) {
+      //super big thanks to Shanqing Cai for help with weights and bias:
+      //https://groups.google.com/a/tensorflow.org/forum/#!topic/tfjs/ORkUHg_k_fU
+      tf.tidy(() => {
+        //get weights and biases as tensor
+        const ih_weights = this.model.layers[0].getWeights()[0]; //tensor shape 2 [5,8]
+        const ho_weights = this.model.layers[1].getWeights()[0]; //tensor shape 2 [8,2]
+        const h_bias = this.model.layers[0].getWeights()[1]; //tensor shape [8]
+        const o_bias = this.model.layers[1].getWeights()[1]; //tensor shape [2]
+
+        //mutate by adding scalar to each tensor
+        // const mutationTensor = tf.scalar(mutationScalar); //needs to be number
+        //have to do weird 1d tensor because can't make tf.scalar with a variable ***
+        //mutate ih_weights and h_bias
+        let hidden_array = [];
+        for (let i = 0; i < this.hidden_nodes; i++) {
+          hidden_array.push(m);
+        }
+        const hidden_mutationTensor = tf.tensor1d(hidden_array);
+        const ih_w_mutated = ih_weights.add(hidden_mutationTensor);
+        const h_b_mutated = h_bias.add(hidden_mutationTensor);
+        //mutate ho_weights and o_bias;
+        let output_array = [];
+        for (let i = 0; i < this.output_nodes; i++) {
+          output_array.push(m);
+        }
+        const output_mutationTensor = tf.tensor1d(output_array);
+        const ho_w_mutated = ho_weights.add(output_mutationTensor);
+        const o_b_mutated = o_bias.add(output_mutationTensor);
+
+        // console.log('weights');
+        // ih_weights.print();
+        // ih_w_mutated.print();
+        // console.log('bias');
+        // h_bias.print();
+        // h_b_mutated.print();
+
+        //now set the weights and bias of layers
+        this.model.layers[0].setWeights([ih_w_mutated, h_b_mutated]);
+        this.model.layers[1].setWeights([ho_w_mutated, o_b_mutated]);
+
+
+
+      });
+    }
+
+    // this.model.layers[0].getWeights()[0].print();
+  }
+}
+
+
+// async function pred(brain, inputs) {
+//   console.log('pred');
+//   return await brain.model.predict(inputs).array; //await?
+// }
+/*
+//from example
+
+//config model
+const model = tf.sequential();
+
+//config each layer
+const configHidden = {
+  inputShape: [5],
+  units: 4,
+  activation: 'sigmoid'
+}
+const hidden = tf.layers.dense(configHidden);
+
+const configOutput = {
+  units: 2,
+  activation: 'sigmoid'
+}
+const output = tf.layers.dense(configOutput);
+
+//add layers to the model
+model.add(hidden);
+model.add(output);
+
+console.log(hidden);
+// hidden.print();
+console.log(output);
+
+const sgdOpt = tf.train.sgd(0.1);
+const config = {
+  optimizer: sgdOpt,
+  loss: 'meanSquaredError'
+}
+model.compile(config);
+
+console.log(model);
+tf.print(model);
+//don't need predict for NeurEvo
+let outputs = model.predict(tf.tensor2d([
+  [0.25, 0.92, 0.3, 0.23, 0.11]
+]));
+console.log(outputs);
+
+//tensors tracked
+console.log(tf.memory());
+
+//which  backend -- webGL
+// console.log(tf.getBackend());
+*/
